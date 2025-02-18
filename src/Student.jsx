04cfa9a28acd6626463d9b2cd9b873937b815e25
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-02-18 23:30:31 +0600

    props send from an object

--- src/Student.jsx
+++ src/Student.jsx
@@ -1,6 +1,9 @@
 /* eslint-disable react/prop-types */
 export default function Students({student}){
     return(
-        <p>Student Name : {student.name} Age: {student.age}</p>
+        <div>
+            <p>Student Name : {student.name} </p>
+            <p>Age : {student.age}</p>
+        </div>
     )
 }
