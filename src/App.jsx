5aae1300e80c5caa49b7cc4fd159a73bf1a5e7e3
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-02-18 23:28:35 +0600

    props send from an object

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
 import './App.css'
 import Singer from './Singer'
+import Students from './Student'
 
 function App() {
 
-  const singers = ['Abul', 'Babul','Bulbul', 'Mofijul']
-  
+  const singers = ['Abul', 'Babul', 'Bulbul', 'Mofijul']
+
+  const students = [
+    {name: 'Raju', age: 20},
+    {name: 'Paju', age: 20},
+    {name: 'Saju', age: 20}
+  ]
 
   return (
     <>
       <h1>new react app</h1>
       <p>Singers Details</p>
       {
-        singers.map((singer,index) => <Singer key={index} singer={singer}></Singer>)
+        singers.map((singer, index) => <Singer key={index} singer={singer}></Singer>)
+      }
+
+      {
+        students.map((student, index) => <Students key={index} student = {student}></Students>)
       }
     </>
   )
---
+++ src/Student.jsx
@@ -0,0 +1,6 @@
+/* eslint-disable react/prop-types */
+export default function Students({student}){
+    return(
+        <p>Student Name : {student.name} Age: {student.age}</p>
+    )
+}
